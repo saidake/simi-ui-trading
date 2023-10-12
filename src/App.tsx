21567c51c55a71c336d321f44d3c41ed4d3fa952
simi <saidake@qq.com>
2023-10-12 11:01:05 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, {Suspense} from "react";
+
+import BasicPage from "@/pages/BasicPage";
+const App: React.FC<any> = () => {
+  return (
+   <BasicPage/>
+  );
+};
+
+export default App;
